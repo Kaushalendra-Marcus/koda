0d50a3e39ba7b0818733c40d70055a5b6483f6b1
Kaushalendra-Marcus <yadavkausha4a5@gmail.com>
2026-07-04 09:28:27 +0530

    updated

--- src/types.ts
+++ src/types.ts
@@ -1,16 +1,16 @@
 type Role = "system" | "user" | "assistant" | "tool";
 
-interface Prompt {
-  role: string;
-  prompt: string;
+interface Message {
+  role: Role;
+  content: string;
 }
-
 
 interface Tool {
   tool_name: string;
   description: string;
   action: string;
 }
+
 interface LLM_Response {
   response: string;
 }
